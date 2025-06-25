8957f7104531e329067764fa3450aed8adf64f50
Kiran George <kiran.george@fissionlabs.com>
2025-06-25 19:27:22 +0530

    fix: change context key

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -78,9 +78,7 @@ class EvaluationRunner:
                 question=record.get('question'),
                 generated_answer=record.get('generated_answer'),
                 expected_answer=record.get('gt_answer'),
-                context=[
-                    ctx.get('content', '') for ctx in record.get('retrieval_context', [])
-                    ]
+                context=record.get('retrieval_contexts', [])
             ) for record in self.metric_records
             ]
             
